9956ddecb829d9c885232cb0dc1c97fa47f196e3
jose guilherme <jgnvs@cin.ufpe.br>
2019-07-18 22:56:05 -0300

    criacao dos componentes navbar,isnertItem e about

---
+++ src/app/insert-item/insert-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-item',
+  templateUrl: './insert-item.component.html',
+  styleUrls: ['./insert-item.component.css']
+})
+export class InsertItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
