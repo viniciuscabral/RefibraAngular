d4b4409a0a7d31e9a2e12425f54081338343bde3
Vinicius Cabral <vviniciuscabral@gmail.com>
2019-10-29 23:52:20 -0300

    ajuste endereco

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   settings: {
-    BACKEND_API_FUSEKI_URL: 'http://localhost:5000',
+    BACKEND_API_FUSEKI_URL: 'http://refibra-api:5000',
     IMAGES_PATH: 'https://res.cloudinary.com/dixelsjzs/image/upload/v1565234304/Refibra/'
   },
   production: false,
